ad9ea8a1b8e430098e511736223dd156c3f38342
syegod <ovisochin362@gmail.com>
2023-10-17 17:14:09 +0200

    user list reworked, eps and rating update

--- src/components/features/AddToList.jsx
+++ src/components/features/AddToList.jsx
@@ -9,13 +9,8 @@ export default function AddToList({ anime }) {
 
     useEffect(() => {
         try {
-            const userlists = Object.entries(userData?.lists);
-            for (var i of userlists) {
-                console.log(i);
-                if (i[1].some(e => e.anime.mal_id === anime.mal_id)) {
-                    setInUserList(i[0]);
-                    break;
-                }
+            if(userData?.list.some(e => e.anime?.mal_id === anime?.mal_id)){
+                setInUserList(userData?.list?.filter(e => e.anime?.mal_id === anime?.mal_id)[0].list);
             }
         } catch (error) {
             console.log(error);
@@ -39,33 +34,39 @@ export default function AddToList({ anime }) {
         <div className='w-full relative text-start'>
             <button className={`${userlist ? `bg-green-400` : `bg-zinc-900`} text-white w-full rounded py-1 active:scale-[100.5%]`} onClick={() => setOpen(!open)}>{userlist ? userlist.charAt(0).toUpperCase() + userlist.slice(1) : 'Add to list'}</button>
             <div className={`flex flex-col w-full divide-y divide-zinc-200 bg-white rounded overflow-hidden ${open ? `` : `hidden`} origin-top duration-100 absolute border-zinc-400 border top-8`}>
-                <button className='py-2 w-full flex flex-row gap-x-2 items-center justify-center' name='watching' disabled={userlist !== null} onClick={e => handleAddToList(e)}>
+                <button className='py-2 w-full flex flex-row gap-x-2 items-center justify-center' name='watching'  onClick={e => handleAddToList(e)}>
                     Watching
                     {userlist === 'watching' && <span class="material-symbols-outlined">
                         done
                     </span>}
                 </button>
-                <button className='py-2 w-full flex flex-row gap-x-2 items-center justify-center' name='planned' disabled={userlist !== null} onClick={e => handleAddToList(e)}>
+                <button className='py-2 w-full flex flex-row gap-x-2 items-center justify-center' name='planned'  onClick={e => handleAddToList(e)}>
                     Planned
                     {userlist === 'planned' && <span class="material-symbols-outlined">
                         done
                     </span>}
                 </button>
-                <button className='py-2 w-full flex flex-row gap-x-2 items-center justify-center' name='viewed' disabled={userlist !== null} onClick={e => handleAddToList(e)}>
+                <button className='py-2 w-full flex flex-row gap-x-2 items-center justify-center' name='viewed'  onClick={e => handleAddToList(e)}>
                     Viewed
                     {userlist === 'viewed' && <span class="material-symbols-outlined">
                         done
                     </span>}
                 </button>
-                <button className='py-2 w-full flex flex-row gap-x-2 items-center justify-center' name='favorite' disabled={userlist !== null} onClick={e => handleAddToList(e)}>
+                <button className='py-2 w-full flex flex-row gap-x-2 items-center justify-center' name='favorite' onClick={e => handleAddToList(e)}>
                     Favorite
                     {userlist === 'favorite' && <span class="material-symbols-outlined">
                         done
                     </span>}
                 </button>
-                <button className='py-2 w-full flex flex-row gap-x-2 items-center justify-center' name='abandoned' disabled={userlist !== null} onClick={e => handleAddToList(e)}>
-                    Abandoned
-                    {userlist === 'abandoned' && <span class="material-symbols-outlined">
+                <button className='py-2 w-full flex flex-row gap-x-2 items-center justify-center' name='dropped' onClick={e => handleAddToList(e)}>
+                    Dropped
+                    {userlist === 'dropped' && <span class="material-symbols-outlined">
+                        done
+                    </span>}
+                </button>
+                <button className='py-2 w-full flex flex-row gap-x-2 items-center justify-center' name='onhold' onClick={e => handleAddToList(e)}>
+                    On hold
+                    {userlist === 'onhold' && <span class="material-symbols-outlined">
                         done
                     </span>}
                 </button>
